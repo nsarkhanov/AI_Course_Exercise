71f8fcec9488bbb96ffdaf40ca4b4edddfd75889
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-06-10 18:09:24 +0400

    update

--- Projects/self_project/learning/colorhsvinvideo.py
+++ Projects/self_project/learning/colorhsvinvideo.py
@@ -8,4 +8,11 @@ video = cv2.VideoCapture(0)
 while True:
     _, frame = video.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imshow('HSV frame', hsv)
+    cv2.imshow('HSV frame', frame)
+
+    k = cv2.waitKey(0) & 0xFF == ord('q')
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+video.release()
