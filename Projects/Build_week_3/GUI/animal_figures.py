aba9385376352662b3896d9fdf53aeb21221664d
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-06-24 18:48:28 +0400

    update

--- Projects/Build_week_3/GUI/animal_figures.py
+++ Projects/Build_week_3/GUI/animal_figures.py
@@ -3,7 +3,7 @@ import numpy as np
 import mediapipe as mp
 
 
-img = np.zeros(shape=[512, 512, 3], dtype=np.uint8)
+img = np.ones(shape=[512, 512, 3], dtype=np.uint8)
 class Draw():
     def __init__(self,hight, width):
         self.width=width
@@ -15,15 +15,16 @@ class Draw():
 
 
 
-dog_head_color=(33,67,101)
-center=img.shape[0]//2,img.shape[1]//2
+color=(33,67,101)
+center_coordinate=img.shape[0]//2,img.shape[1]//2
+head_size=20
 
 #cv2.ellipse(img,bottum_center,(len_bottom,int(len_bottom*0.6)),angle_bottom,0,180,dog_head_color,-1)
 #cv2.ellipse(img,top_center,(len_top,int(len_top*0.35)),angle_top,0,360,dog_head_color,-1)
 #cv2.ellipse(img,head_top,(int(len_top*(2/3)),int(len_top*0.6)),angle_top+5,180,360,dog_head_color,-1)
 #cv2.ellipse(img,(head_top[0]+40,head_top[1]-int(len_top*(2/5))),(int(len_top*0.25),int(len_top*0.65)),angle_top,180,360,dog_head_color,-1)
 #eyes
-cv2.circle(img,center,12,(255,255,255),cv2.FILLED)
+cv2.circle(img,center_coordinate,head_size,color,cv2.FILLED)
 
 
 
