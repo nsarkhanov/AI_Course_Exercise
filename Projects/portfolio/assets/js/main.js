c9efdffa4a735969ab2f0cffc5e58f84c0027855
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-07-09 07:21:44 +0200

    update

--- Projects/portfolio/assets/js/main.js
+++ Projects/portfolio/assets/js/main.js
@@ -1,2 +1,20 @@
+function checkdata()
+            {
+                var username = document.getElementById("name");
+                var emailid = document.getElementById("email");
+                if(username.value==""){
+                    alert("Please enter the name");
+                    username.focus();
+                    return false;
+                    }
+                    if(emailid.value == ""){
+                        alert("Please enter the email");
+                        emailid.focus();
+                        return false;
+                  }
+                 //If all is well return true.
+                  return true;
 
+
+            }
 
