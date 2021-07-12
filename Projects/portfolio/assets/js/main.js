6f925af34c2644f44955041a973413dd42e6ce67
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-07-12 09:55:24 +0200

    update

--- Projects/portfolio/assets/js/main.js
+++ Projects/portfolio/assets/js/main.js
@@ -1,20 +1,39 @@
-function checkdata()
-            {
-                var username = document.getElementById("name");
-                var emailid = document.getElementById("email");
-                if(username.value==""){
-                    alert("Please enter the name");
-                    username.focus();
-                    return false;
-                    }
-                    if(emailid.value == ""){
-                        alert("Please enter the email");
-                        emailid.focus();
-                        return false;
-                  }
-                 //If all is well return true.
-                  return true;
-
-
-            }
-
+// ---------Responsive-navbar-active-animation-----------
+function test(){
+    var tabsNewAnim = $('#navbarSupportedContent');
+    var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+    var activeItemNewAnim = tabsNewAnim.find('.active');
+    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+    var itemPosNewAnimTop = activeItemNewAnim.position();
+    var itemPosNewAnimLeft = activeItemNewAnim.position();
+    $(".hori-selector").css({
+      "top":itemPosNewAnimTop.top + "px", 
+      "left":itemPosNewAnimLeft.left + "px",
+      "height": activeWidthNewAnimHeight + "px",
+      "width": activeWidthNewAnimWidth + "px"
+    });
+    $("#navbarSupportedContent").on("click","li",function(e){
+      $('#navbarSupportedContent ul li').removeClass("active");
+      $(this).addClass('active');
+      var activeWidthNewAnimHeight = $(this).innerHeight();
+      var activeWidthNewAnimWidth = $(this).innerWidth();
+      var itemPosNewAnimTop = $(this).position();
+      var itemPosNewAnimLeft = $(this).position();
+      $(".hori-selector").css({
+        "top":itemPosNewAnimTop.top + "px", 
+        "left":itemPosNewAnimLeft.left + "px",
+        "height": activeWidthNewAnimHeight + "px",
+        "width": activeWidthNewAnimWidth + "px"
+      });
+    });
+  }
+  $(document).ready(function(){
+    setTimeout(function(){ test(); });
+  });
+  $(window).on('resize', function(){
+    setTimeout(function(){ test(); }, 500);
+  });
+  $(".navbar-toggler").click(function(){
+    setTimeout(function(){ test(); });
+  });
